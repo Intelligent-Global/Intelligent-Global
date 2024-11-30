1c707cf19eed4bf89badc5d6497dd97654a72b55
Intelligent-Global <intelglobal28@gmail.com>
2024-11-30 18:57:26 +0700

    Filter Enabled

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -68,6 +68,7 @@ export class AppComponent {
         this.images = this.data?.images
         this.arrays = this.data?.arrays
         this.arrays.array001 = this.showAdminButton() ? this.arrays?.array001 : this.arrays?.array001?.filter?.((x: any) => x?.name !== 'การจัดการข้อมูล')
+        this.arrays.array002 = this.arrays?.array002?.filter?.((x: any) => x?.enabled)
         this.arrays.array002?.forEach((element: any) => {
           element.name = '<li>' + element?.name + '</li>'
         });
